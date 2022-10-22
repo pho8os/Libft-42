87d8fd16b31825d9a7e3dd43298572119048c44a
Abderrahim Said <absaid@student.1337.ma>
2022-10-22 17:41:05 +0100

    bankai

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: absaid <absaid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 07:27:06 by absaid            #+#    #+#             */
-/*   Updated: 2022/10/22 16:47:49 by absaid           ###   ########.fr       */
+/*   Updated: 2022/10/22 17:38:38 by absaid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	slen = ft_strlen(s);
+	slen = ft_strlen(s + start);
 	i = 0;
-	while (i != start)
-		i++;
 	if (len > slen)
-		len = slen - i;
-	str = malloc(len + 1 * sizeof (char));
+		len = slen;
+	str = malloc(len + 1);
 	if (!str)
 		return (NULL);
+	slen = ft_strlen(s);
 	i = -1;
 	while (++i < len && start[s] && start < slen)
 		*(str + i) = *(s + start + i);
