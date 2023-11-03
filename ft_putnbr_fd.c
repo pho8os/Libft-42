f219233fe5536d282c39b6579e3d5a32b5cee000
Abderrahim Said <104790615+pho8os@users.noreply.github.com>
2023-11-03 04:50:13 +0100

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -17,12 +17,7 @@ void	ft_putnbr_fd(int n, int fd)
 	long int	nb;
 
 	nb = n;
-	if (nb < 0)
-	{
-		nb *= -1;
-		write(fd, "-", 1);
-	}
-	if (nb > 9)
-		ft_putnbr_fd(nb / 10, fd);
+	(nb < 0) && (write(fd, "-", 1), nb *= -1);
+	(nb > 9) && (ft_putnbr_fd(nb / 10, fd), 0);
 	write(fd, &"0123456789"[nb % 10], 1);
 }
