32924df8adebeeb2773be3c041f899c4b91717cf
Abderrahim Said <104790615+pho8os@users.noreply.github.com>
2024-02-16 19:19:28 +0100

    Update ft_lstadd_back.c

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -17,10 +17,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	t_list	*tmp;
 
 	if (!*lst)
-	{
-		*lst = new;
-		return ;
-	}
+		return (*lst = new, (void)0);
 	tmp = *lst;
 	while (tmp -> next)
 		tmp = tmp -> next;
